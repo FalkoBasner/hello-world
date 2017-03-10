a67ec05e02d2c0727f1b345b1ed43df179743302
FalkoBasner <falko.basner@gmx.net>
2017-03-10 11:36:58 +0100

    add jenkins pipeline

--- src/main/java/de/is24/cloud/configuration/MessagingConfig.java
+++ src/main/java/de/is24/cloud/configuration/MessagingConfig.java
@@ -1,5 +1,12 @@
 package de.is24.cloud.configuration;
 
+import static de.is24.cloud.configuration.AppEnvironment.getStackName;
+import static de.is24.cloud.configuration.AppEnvironment.isDevProfile;
+import static de.is24.cloud.configuration.AppEnvironment.sqsHost;
+import static de.is24.cloud.configuration.AppEnvironment.sqsPort;
+import static java.lang.String.format;
+import static org.apache.commons.lang3.StringUtils.EMPTY;
+
 import com.amazonaws.ClientConfiguration;
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.regions.Region;
@@ -15,64 +22,62 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.core.env.Environment;
 
-import static de.is24.cloud.configuration.AppEnvironment.*;
-import static java.lang.String.format;
-import static org.apache.commons.lang3.StringUtils.EMPTY;
-
 
 @Configuration
 @Import(AwsConfig.class)
 @Slf4j
 public class MessagingConfig {
-	private static final Regions REGION = Regions.EU_WEST_1;
-	private static final String SQS_SERVICE_NAME = "sqs";
-	private static final String INSTANCE_QUEUE = "instances";
 
-	@Autowired
-	private Environment environment;
-	@Autowired
-	private AWSCredentialsProvider awsCredentialsProvider;
-	@Autowired
-	private ClientConfiguration clientConfiguration;
+  private static final Regions REGION = Regions.EU_WEST_1;
+  private static final String SQS_SERVICE_NAME = "sqs";
+  private static final String INSTANCE_QUEUE = "instances";
 
-	@Bean
-	public AmazonSQS amazonSQSClient() {
-		final AmazonSQSClient amazonSQSClient = new AmazonSQSClient(awsCredentialsProvider, clientConfiguration);
-		if (isDevProfile(environment)) {
-			final String endpoint = format("http://%s:%s", sqsHost(environment), sqsPort(environment));
-			log.info("using endpoint {}", endpoint);
-			amazonSQSClient.setEndpoint(endpoint, SQS_SERVICE_NAME, EMPTY);
-		} else {
-			amazonSQSClient.setRegion(Region.getRegion(REGION));
-		}
-		return amazonSQSClient;
-	}
+  @Autowired
+  private Environment environment;
+  @Autowired
+  private AWSCredentialsProvider awsCredentialsProvider;
+  @Autowired
+  private ClientConfiguration clientConfiguration;
 
-	@Bean
-	public SqsQueue instanceQueue() {
-		return getSqsQueue(INSTANCE_QUEUE);
-	}
+  @Bean
+  public AmazonSQS amazonSQSClient() {
+    final AmazonSQSClient amazonSQSClient = new AmazonSQSClient(awsCredentialsProvider,
+        clientConfiguration);
+    if (isDevProfile(environment)) {
+      final String endpoint = format("http://%s:%s", sqsHost(environment), sqsPort(environment));
+      log.info("using endpoint {}", endpoint);
+      amazonSQSClient.setEndpoint(endpoint);
+    } else {
+      amazonSQSClient.setRegion(Region.getRegion(REGION));
+    }
+    return amazonSQSClient;
+  }
 
-	private SqsQueue getSqsQueue(final String queueName) {
-		final String url = findOrCreate(queueName);
-		return new SqsQueue(url, amazonSQSClient());
-	}
+  @Bean
+  public SqsQueue instanceQueue() {
+    return getSqsQueue(INSTANCE_QUEUE);
+  }
 
-	private String findOrCreate(final String queueName) {
-		final String sqsPrefix;
-		if (isDevProfile(environment)) {
-			sqsPrefix = EMPTY;
-		} else {
-			sqsPrefix = getStackName(environment).get() + "-";
-		}
+  private SqsQueue getSqsQueue(final String queueName) {
+    final String url = findOrCreate(queueName);
+    return new SqsQueue(url, amazonSQSClient());
+  }
 
-		final String awsQueueName = sqsPrefix + queueName;
+  private String findOrCreate(final String queueName) {
+    final String sqsPrefix;
+    if (isDevProfile(environment)) {
+      sqsPrefix = EMPTY;
+    } else {
+      sqsPrefix = getStackName(environment).get() + "-";
+    }
 
-		log.info("find or create sqs queue: {}", awsQueueName);
+    final String awsQueueName = sqsPrefix + queueName;
 
-		final CreateQueueRequest createQueueRequest = new CreateQueueRequest(awsQueueName);
-		final String queueUrl = amazonSQSClient().createQueue(createQueueRequest).getQueueUrl();
-		log.info("sqs queue url: {}", queueUrl);
-		return queueUrl;
-	}
+    log.info("find or create sqs queue: {}", awsQueueName);
+
+    final CreateQueueRequest createQueueRequest = new CreateQueueRequest(awsQueueName);
+    final String queueUrl = amazonSQSClient().createQueue(createQueueRequest).getQueueUrl();
+    log.info("sqs queue url: {}", queueUrl);
+    return queueUrl;
+  }
 }
